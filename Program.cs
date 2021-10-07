7c8002f085289d276dcbaf448a89a0b2da793ea3
evelazco <evelazco@cimex.com.cu>
2021-10-07 14:28:55 -0500

    Ok

--- Program.cs
+++ Program.cs
@@ -11,46 +11,80 @@ namespace Tries
         {
             Console.WriteLine("Tries Started!");
 
-            StringBuilder feed = new StringBuilder();
+            InitDB();
 
-            while (feed.ToString() != "exit")
+            StringBuilder feed = new();
+            ConsoleKeyInfo key = default(ConsoleKeyInfo);
+
+            do
             {
-                ConsoleKeyInfo k = new ConsoleKeyInfo();
-                while (true)
+                key = Console.ReadKey(true);
+
+                if (key.Key == ConsoleKey.Tab)
+                    feed.Append(GetProposals(feed.ToString()));
+                else if (key.Key == ConsoleKey.Enter)
                 {
-                    k = Console.ReadKey(false);
-                    if (k.Key == ConsoleKey.Enter)
+                    if (feed.Length > 0)
+                        trie.AddString(feed.ToString());
+                    Console.WriteLine();
+                    feed.Clear();
+                }
+                else if (key.Key == ConsoleKey.Backspace)
+                {
+                    if (Console.CursorLeft > 0)
                     {
-                        Console.Write("\n");
-                        break;
+                        Console.CursorLeft--;
+                        Console.Write(" ");
+                        Console.CursorLeft--;
+                        feed.Remove(feed.Length - 1, 1);
                     }
-                    if (!Char.IsPunctuation(k.KeyChar))
-                        feed.Append(k.KeyChar);
-
                 }
-
-                if (feed.ToString() != "exit")
-                    trie.AddString(feed.ToString());
                 else
-                    break;
-
-                feed.Clear();
-            }
-
-            string temp = string.Empty;
-            while (temp != "exit")
-            {
-                Console.WriteLine("Gimme template!");
-                temp = Console.ReadLine();
-                var options = trie.GetStrings(temp);
-                foreach (var item in options)
                 {
-                    Console.WriteLine(item.ToString());
+                    feed.Append(key.KeyChar);
+                    Console.Write(key.KeyChar);
                 }
-            }
 
-            Console.WriteLine();
-            Console.WriteLine($"That's all!");
+
+
+            } while (feed.ToString() != "exit");
+
+        }
+
+        static void InitDB()
+        {
+            string[] db = { "dir", "del", "do", "delete" };
+
+            foreach (string item in db)
+                trie.AddString(item);
+        }
+
+        static string GetProposals(string str)
+        {
+            var proposals = trie.GetStrings(str);
+
+            if (proposals.Count == 1)
+            {
+                Console.Write(proposals[0].ToString().Replace(str, ""));
+                return proposals[0].ToString().Replace(str, "");
+            }
+            else if (proposals.Count > 1)
+            {
+                Console.WriteLine();
+                for (int i = 0; i < proposals.Count; i++)
+                {
+                    Console.Write(proposals[i].ToString());
+
+                    if ((i + 1) % 4 == 0)
+                        Console.WriteLine();
+                    else
+                        Console.Write("\t\t");
+                }
+                Console.WriteLine();
+                Console.Write(str);
+                return string.Empty;
+            }
+            return string.Empty;
         }
     }
 }
